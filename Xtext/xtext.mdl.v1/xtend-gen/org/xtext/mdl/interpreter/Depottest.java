8f4d39ed934deecdea735b95d5e2dbfe5fd7fabf
17010315 <christian.sourisseau@etudiant.univ-rennes1.fr>
2020-02-08 14:26:34 +0100

    [FIX] Xtext/ Tentative de fix le projet Xtext

---
+++ Xtext/xtext.mdl.v1/xtend-gen/org/xtext/mdl/interpreter/Depottest.java
@@ -0,0 +1,7 @@
+package org.xtext.mdl.interpreter;
+
+import org.xtext.mdl.v1.impl.DepotImpl;
+
+@SuppressWarnings("all")
+public class Depottest extends DepotImpl {
+}
---
+++ Xtext/xtext.mdl.v1/xtend-gen/org/xtext/mdl/interpreter/QCMTest.java
@@ -0,0 +1,7 @@
+package org.xtext.mdl.interpreter;
+
+import org.xtext.mdl.v1.impl.QCMImpl;
+
+@SuppressWarnings("all")
+public class QCMTest extends QCMImpl {
+}
