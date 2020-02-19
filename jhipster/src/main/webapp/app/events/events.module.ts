c9c1b4d7c1dbec8a0acf44fae1e04b76e295b209
Thomas.Esseul <esseul.tho@gmail.com>
2020-02-19 16:38:32 +0100

    [FIX] Retrait d'includes non utilisées

--- jhipster/src/main/webapp/app/events/events.module.ts
+++ jhipster/src/main/webapp/app/events/events.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MdlSharedModule } from 'app/shared/shared.module';
 import { RouterModule } from '@angular/router';
