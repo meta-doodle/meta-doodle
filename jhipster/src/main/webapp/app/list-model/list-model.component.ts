85877013dd89bb80a0a9052387730091269f752b
17012190 <Emmanuel Roncier" emmanuel.roncier@etudiant.univ-rennes1.fr>
2020-04-13 17:40:31 +0200

    [FONC] Front : Creation WF instance

--- jhipster/src/main/webapp/app/list-model/list-model.component.ts
+++ jhipster/src/main/webapp/app/list-model/list-model.component.ts
@@ -8,24 +8,6 @@ import { WorkflowModelService } from '../entities/workflow-model/workflow-model.
   styleUrls: ['list-model.component.scss']
 })
 export class ListModelComponent implements OnInit {
-  models = [
-    {
-      id: 1,
-      title: 'Soirée Pizza entre amis ',
-      description: "Modele Type : Choix de date et choix d'une pizzeria"
-    },
-    {
-      id: 2,
-      title: 'La révolution type entre amis',
-      description: 'Modele Type : Organisation du renversement du gouvernement'
-    },
-    {
-      id: 3,
-      title: 'Coronavirus : Qui veut des masques ? ',
-      description: 'Modele Type : Organisation de la fabrication de masque et répartition des lieux de distribution'
-    }
-  ];
-
   modelsType: any;
 
   constructor(private service: WorkflowModelService) {}
