9ea4754ccef1a203b8d6205872d6a613c71b5341
Nathan <nathan.rio@etudiant.univ-rennes1.fr>
2020-05-14 18:24:00 +0200

    [FONC] Prepare l'envoie de mail de rappel

--- jhipster/src/main/java/org/jhipster/mdl/interpreter/InterpreterInterface.java
+++ jhipster/src/main/java/org/jhipster/mdl/interpreter/InterpreterInterface.java
@@ -1,6 +1,12 @@
 package org.jhipster.mdl.interpreter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.jhipster.mdl.domain.CurrentStep;
 import org.jhipster.mdl.domain.MdlUser;
@@ -8,6 +14,7 @@ import org.jhipster.mdl.domain.WorkflowInstanceState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xtext.metadoodle.interpreter.Interface.Interpreter;
+import org.xtext.metadoodle.interpreter.Interface.MailReminder;
 import org.xtext.metadoodle.interpreter.Interface.StepDTO;
 import org.xtext.metadoodle.interpreter.Interface.StepDTOFactory;
 import org.xtext.metadoodle.interpreteur.implementation.InterpreterImpl;
@@ -45,9 +52,43 @@ public class InterpreterInterface {
 			
 		}
 		
+		if(wfStepFactory.getMailReminder().isPresent()) {
+			MailReminder mailReminder = wfStepFactory.getMailReminder().get();
+			mailReminder.getDateToSend().forEach(date -> newMailSender(date, mailReminder, mdlUser));
+		}
+		
 		return Optional.of(wfStepFactory.getStepDTO());
 	}
 	
+	private static void newMailSender(String date, MailReminder mailReminder, MdlUser mdlUser) {
+		log.info("Prepare to send mail at {}" , date);
+		
+		try {
+			Date d = new SimpleDateFormat("dd/MM/yy").parse(date);
+			Timer timer = new Timer(true);
+			Date now = Date.from(Instant.now());
+			if(now.before(d) || now.equals(d)) {
+				log.debug("deamon launch");
+				timer.schedule(new TimerTask() {
+					
+					@Override
+					public void run() {
+						log.info("send mail");
+						sendMail(mailReminder.getObject(), mailReminder.getBody(), mdlUser.getUser().getEmail());
+					}
+				}, d);
+			}
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+
+	private static void sendMail(String object, String body, String mailAddress) {
+		// TODO
+	}
+
 	public static String getStepIdent(String wfModel) {
 		return INTERPRETER.getWorkflowData(wfModel).getIDFirstStep();
 	}
