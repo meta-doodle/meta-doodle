6306bf487cfbea291f1f601f75ef85a81dcf9c13
Nathan <nathan.rio@etudiant.univ-rennes1.fr>
2020-05-19 17:46:48 +0200

    [FIX] ajout imports

--- jhipster/src/main/java/org/jhipster/mdl/repository/RoleRepository.java
+++ jhipster/src/main/java/org/jhipster/mdl/repository/RoleRepository.java
@@ -1,14 +1,16 @@
 package org.jhipster.mdl.repository;
 
+import java.util.Optional;
+
 import org.jhipster.mdl.domain.Role;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 /**
  * Spring Data  repository for the Role entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
