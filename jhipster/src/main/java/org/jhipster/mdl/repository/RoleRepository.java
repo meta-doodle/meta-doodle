3ff0003a7ed47480ecad5dd4efb40a49f56e46e7
Nathan <nathanjojorio@gmail.com>
2020-05-20 00:42:55 +0200

    [FIX] SQL request

--- jhipster/src/main/java/org/jhipster/mdl/repository/RoleRepository.java
+++ jhipster/src/main/java/org/jhipster/mdl/repository/RoleRepository.java
@@ -2,9 +2,10 @@ package org.jhipster.mdl.repository;
 
 import java.util.Optional;
 
+import org.jhipster.mdl.domain.MdlUser;
 import org.jhipster.mdl.domain.Role;
+import org.jhipster.mdl.domain.WorkflowInstance;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -14,7 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-	@Query("SELECT DISTINCT role FROM Role r WHERE r.user = userId and r.workflowInstance = wfId")
-	Optional<Role> findUsersRoleInWorkflow(@Param("userId") Long userId, @Param("wfId") Long wfId);
+	Optional<Role> findDistinctRoleByWorkflowInstanceIdAndUserId(Long wfId, Long userId);
 
 }
